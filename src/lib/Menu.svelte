e6b22a97cd26319070a5aaccd9df86ca6c818251
Amerlander <Amerlander@users.noreply.github.com>
2023-10-27 16:03:43 +0200

    update ssr

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,152 +1,117 @@
-<script type="ts">
-  type MenuPosition = [number, number] | null;
-
-  /**
-   * Once a menu has been made visible, these events will trigger
-   * the close() function
-   */
-  const CLOSE_LISTENERS = {
-      "click": document.body,
-      "contextmenu": document.body,
-      "scroll": document
-  };
-
-  import { onDestroy, onMount, setContext } from "svelte";
-  import { writable } from "svelte/store";
-
-  import type { ContextMenuMouseEvent } from "./ContextMenuMouseEvent";
-
-  import type Settings from "./Settings";
-  import { createStyleString, findParentWithScroll } from "./utilities";
-  import { currentMenu, defaultSettings } from "./stores";
-  import { computeMenuStyle, getMenuClass } from "./menuUtilities";
-
-  /** Menu Settings */
-  export let settings: Settings = null;
-  const finalSettings = writable(settings || $defaultSettings);
-
-  $: {
+<script>/**
+ * Once a menu has been made visible, these events will trigger
+ * the close() function
+ */
+import { onDestroy, onMount, setContext } from "svelte";
+import { get, writable } from "svelte/store";
+import { createStyleString, findParentWithScroll, assignEventTarget } from "./utilities";
+import { currentMenu, defaultSettings, CLOSE_LISTENERS } from "./stores";
+import { computeMenuStyle, getMenuClass } from "./menuUtilities";
+/** Menu Settings */
+export let settings = null;
+const finalSettings = writable(settings || $defaultSettings);
+$: {
     // Update settings on change
     $finalSettings = settings || $defaultSettings;
     setContext("settings", finalSettings);
-  }
-
-  /** Menu State */
-  let menuPosition: MenuPosition = null;
-  $: menuClass = getMenuClass(menuPosition, $finalSettings);
-  $: menuStyle = computeMenuStyle(menuPosition);
-
-  let currentParentContainer : HTMLElement | null = null;
-  let ref : any;
-
-  const currentMenuUnsub = currentMenu.subscribe(value => {
+}
+/** Menu State */
+let menuPosition = null;
+$: menuClass = getMenuClass(menuPosition, $finalSettings);
+$: menuStyle = computeMenuStyle(menuPosition);
+let currentParentContainer = null;
+let ref;
+const currentMenuUnsub = currentMenu.subscribe(value => {
     // If the menu is currently active, but another menu has popped up
     // then close this menu
     if ((menuPosition) && (value !== ref)) {
-      close();
+        close();
     }
-  });
-
-  /**
-   * Given a click event path, get the immediate child of the ContextMenu
-   * that was clicked
-   * @param {EventTarget[]} path
-   */
-  function getImmediateChild(path?: EventTarget[]) {
-    if (path === null || path === undefined) return null;
-
+});
+/**
+ * Given a click event path, get the immediate child of the ContextMenu
+ * that was clicked
+ * @param {EventTarget[]} path
+ */
+function getImmediateChild(path) {
+    if (path === null || path === undefined)
+        return null;
     const ctxMenuIdx = path.indexOf(ref);
-    return path[ctxMenuIdx - 1] as HTMLElement;
-  }
-
-  function close(e?: ContextMenuMouseEvent) {
-    if (e?.contextMenuHandled) return;
-
+    return path[ctxMenuIdx - 1];
+}
+function close(e) {
+    if (e?.contextMenuHandled)
+        return;
     // Automatically close context menu if any child is clicked EXCEPT
     // items with the "autoclose" attribute set to false
     const autoclose = getImmediateChild(e?.composedPath())?.dataset['autoclose'];
     if (autoclose === "false")
-      return;
-
+        return;
     menuPosition = null;
-
     Object.keys(CLOSE_LISTENERS).forEach((eventName) => {
-      const target = CLOSE_LISTENERS[eventName];
-      target.removeEventListener(eventName, close);
+        const target = assignEventTarget(get(CLOSE_LISTENERS)[eventName]);
+        target.removeEventListener(eventName, close);
     });
-
     if (currentParentContainer !== null) {
-      currentParentContainer.removeEventListener("scroll", close);
-      currentParentContainer = null;
+        currentParentContainer.removeEventListener("scroll", close);
+        currentParentContainer = null;
     }
-  }
-
-  /**
-   * Create an event handler, for mouse events, that
-   * activates the context menu
-   */
-  export function createHandler() {
-    return (event: ContextMenuMouseEvent) => show(event);
-  }
-
-  export function show(clickEvent: ContextMenuMouseEvent) {
+}
+/**
+ * Create an event handler, for mouse events, that
+ * activates the context menu
+ */
+export function createHandler() {
+    return (event) => show(event);
+}
+export function show(clickEvent) {
     const { pageX, pageY } = clickEvent;
     clickEvent.contextMenuHandled = true;
-
     menuPosition = [pageX, pageY];
-
     /*
      * This is a hack so we can get dimensions of the menu.
      * This will not cause the menu to render.
      * See: https://stackoverflow.com/questions/35519137/how-do-you-find-the-dimensions-of-a-display-none-element
      */
     ref.style = "display: block";
-
     let refDims = ref.getBoundingClientRect();
     if (pageX + refDims.width > window.innerWidth) {
-      // Will overflow to the right
-      menuPosition = [pageX - refDims.width, pageY];
+        // Will overflow to the right
+        menuPosition = [pageX - refDims.width, pageY];
     }
     else if (pageY + refDims.height > window.innerHeight) {
-      menuPosition = [pageX, pageY - refDims.height]
+        menuPosition = [pageX, pageY - refDims.height];
     }
-
     clickEvent.preventDefault();
     currentMenu.set(ref);
-
     Object.keys(CLOSE_LISTENERS).forEach((eventName) => {
-      const target = CLOSE_LISTENERS[eventName];
-      target.addEventListener(eventName, close);
+        const target = assignEventTarget(get(CLOSE_LISTENERS)[eventName]);
+        target.addEventListener(eventName, close);
     });
-
     // If a parent container w/ custom scrolling exists, then 
     // we attach a scroll listener there as well
     currentParentContainer = findParentWithScroll(clickEvent);
     if (currentParentContainer !== null) {
-      currentParentContainer.addEventListener("scroll", close);
+        currentParentContainer.addEventListener("scroll", close);
     }
-  }
-
-  onMount(() => {
+}
+onMount(() => {
     let portal = document.getElementById("context-menu-portal");
-
     if (portal === null) {
-      portal = document.createElement('div');
-      portal.id = "context-menu-portal";
-      portal.setAttribute("style", createStyleString({
-        position: "absolute",
-        top: 0,
-        left: 0
-      }));
-      document.body.append(portal);
+        portal = document.createElement('div');
+        portal.id = "context-menu-portal";
+        portal.setAttribute("style", createStyleString({
+            position: "absolute",
+            top: 0,
+            left: 0
+        }));
+        document.body.append(portal);
     }
-
     portal.appendChild(ref);
-  });
-
-  onDestroy(() => {
+});
+onDestroy(() => {
     currentMenuUnsub();
-  });
+});
 </script>
 
 <ul class={menuClass} style={menuStyle} bind:this={ref}>
--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -1,39 +1,41 @@
-import type { ContextMenuMouseEvent } from "./ContextMenuMouseEvent";
-
 /** Create a style string from a mapping of keys to values */
-export function createStyleString(style: {[key: string]: string | number}) {
+export function createStyleString(style) {
   const ret = [];
-
   for (const key of Object.keys(style)) {
-    ret.push(`${key}: ${style[key]};`);
+      ret.push(`${key}: ${style[key]};`);
   }
-
   return ret.join(' ');
 }
-
 /**
- * Given a click event on an element, find a parent element that has custom 
- * overflow handling so we can attach a scroll listener to it
- * 
- * @param clickEvent
- * @returns 
- */
-export function findParentWithScroll(clickEvent: ContextMenuMouseEvent) {
-  const target = clickEvent.target as HTMLElement;
-
+* Given a click event on an element, find a parent element that has custom
+* overflow handling so we can attach a scroll listener to it
+*
+* @param clickEvent
+* @returns
+*/
+export function findParentWithScroll(clickEvent) {
+  const target = clickEvent.target;
   let current = target.parentElement;
   while (current !== null && current !== document.body) {
-    const computedStyle = window.getComputedStyle(current);
-    const canScroll = computedStyle.overflowX === "scroll" || computedStyle.overflowY === "scroll";
-    const isOverflown = (current.scrollHeight > current.offsetHeight) ||
-      (current.scrollWidth > current.offsetWidth);
-
-    if (canScroll || isOverflown) {
-      return current;
-    }
-
-    current = current.parentElement;
+      const computedStyle = window.getComputedStyle(current);
+      const canScroll = computedStyle.overflowX === "scroll" || computedStyle.overflowY === "scroll";
+      const isOverflown = (current.scrollHeight > current.offsetHeight) ||
+          (current.scrollWidth > current.offsetWidth);
+      if (canScroll || isOverflown) {
+          return current;
+      }
+      current = current.parentElement;
   }
-
   return null;
+}
+/** Returns document element for event target string */
+export function assignEventTarget(targetString) {
+switch (targetString) {
+  case "document.body":
+    return document.body;
+  case "document":
+    return document;
+  default:
+    break;
+}
 }
